21cb8d297943c246b5eff227ee3e30753358ef85
Barsukov Sergey <BraveBars@gmail.com>
2025-05-25 04:32:44 +0300

    feat(task_2): add User entity and Hibernate configuration

---
+++ src/main/java/com/homework/task_2/User.java
@@ -0,0 +1,76 @@
+package com.homework.task_2;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Имя не может быть пустым")
+    @Column(nullable = false)
+    private String name;
+
+    @Email(message = "Некорректный формат email")
+    @NotBlank(message = "Email не может быть пустым")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Min(value = 0, message = "возраст не может быть отрицательным")
+    @Max(value = 150, message = "возраст не может быть больше 150 лет")
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(nullable = false)
+    private LocalDateTime created_at;
+
+    public User(){};
+
+    public User(String name, String email, Integer age, LocalDateTime created_at) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.created_at = created_at;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+}
