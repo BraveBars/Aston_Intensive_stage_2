5b92d35669815748bec329191356e0b56d4a935b
Barsukov Sergey <BraveBars@gmail.com>
2025-06-08 05:43:34 +0300

    feat(task_4): add UserServiceImpl

--- src/main/java/com/homework/task_4/service/UserServiceImpl.java
+++ src/main/java/com/homework/task_4/service/UserServiceImpl.java
@@ -1,34 +1,53 @@
 package com.homework.task_4.service;
 
+import com.homework.task_4.entity.UserEntity;
+import com.homework.task_4.mapper.UserMapper;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 import com.homework.task_4.dto.UserDto;
+import com.homework.task_4.repository.UserRepository;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class UserServiceImpl implements UserService{
-    private final UserReopsitory repo;
+    private final UserRepository repo;
+
+    public UserServiceImpl(UserRepository repo) {
+        this.repo = repo;
+    }
 
     @Override
     public List<UserDto> findAll() {
-        return List.of();
+        return repo.findAll().stream()
+                .map(UserMapper::toDto)
+                .toList();
     }
 
     @Override
     public UserDto findById(Long id) {
-        return null;
+        return repo.findById(id)
+                .map(UserMapper::toDto)
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 
     @Override
     public UserDto create(UserDto dto) {
-        return null;
+        UserEntity e = UserMapper.toEntity(dto);
+        return UserMapper.toDto(repo.save(e));
     }
 
     @Override
     public UserDto update(Long id, UserDto dto) {
-        return null;
+        UserEntity e = repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        UserMapper.updateEntity(e, dto);
+        return UserMapper.toDto(repo.save(e));
     }
 
     @Override
     public void delete(Long id) {
-
+        repo.deleteById(id);
     }
 }
